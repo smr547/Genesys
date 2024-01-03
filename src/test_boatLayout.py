8cd913e1df19b0350c077445d35bf0ee1aa13f4b
Steven Ring <smr@southsky.com.au>
2024-01-03 14:31:03 +1100

    Futher testing

--- src/test_boatLayout.py
+++ src/test_boatLayout.py
@@ -4,6 +4,7 @@ import json
 # import Space, Access, AccessType, Region
 from Space import * 
 from Region import * 
+from AccessType import * 
 from R1 import * 
 from R2 import * 
 from R3 import * 
@@ -18,6 +19,8 @@ if __name__ == "__main__":
     R3.load_db()
 
     print("Database loaded")
+
+    # Create Regions
 
     Region(None, "Shore").save()
     Region(None, "Top deck").save()
@@ -39,14 +42,47 @@ if __name__ == "__main__":
     Region(None, "Stb Lazarette").save()
     Region(None, "Landing").save()
 
-    r = Region.search("Shore")[0]
-    print(r.__str__())
+    # Create access types
 
-    s = Space(None, "Shore")
-    if not s.save():
-        print("Errors: ", s.errors)
+    accessTypes = ["Open", "Doorway", "Buttoned door", "Door", "Hatch", "Floor cover", "Drawer", \
+        "Seat hatch", "Under bunk hatch", "Backrest cushion", "Seat cushion", "Passerelle"]
+    for at_name in accessTypes:
+       at = AccessType(None, at_name)
+       if not at.save() :
+           print("Errors: ", at.errors)
 
-    r2 = R2(None, s.id, r.id)
+    shoreRegion = Region.search("Shore")[0]
+    print(shoreRegion.__str__())
+
+    shore = Space(None, "Shore")
+    if not shore.save():
+        print("Errors: ", shore.errors)
+
+    r2 = R2(None, shore.id, shoreRegion.id)
     if not r2.save():
         print("Errors: ", r2.errors)
    
+    transomRegion = Region.search("Transom")[0]
+    print(transomRegion.__str__())
+
+    swimDeck = Space(None, "Swim deck")
+    if not swimDeck.save():
+        print("Errors: ", swimDeck.errors)
+
+    r2 = R2(None, swimDeck.id, transomRegion.id)
+    if not r2.save():
+        print("Errors: ", r2.errors)
+
+    # shore to swimdeck access
+
+    passerelle = AccessType.search("Passerelle")[0] 
+
+    passerelleAccess = R1(None, shore.id, swimDeck.id, passable=True)
+    if not passerelleAccess.save():
+        print("Errors: ", passerelleAccess.errors)
+
+    r3 = R3(None, passerelle.id, passerelleAccess.id)
+    if not r3.save():
+        print("Errors: ", r3.errors)
+   
+      
